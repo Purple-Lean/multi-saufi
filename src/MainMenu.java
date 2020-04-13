fb0172ad7b42cbeeaf22e3d132613941feae2276
matpotat <brinkmann-martin@gmx.net>
2020-04-13 19:02:59 +0200

    Wololo

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,6 +1,9 @@
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Cursor;
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.Point;
@@ -9,7 +12,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 
+import javax.swing.BorderFactory;
+import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.GroupLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComponent;
@@ -31,46 +37,42 @@ public class MainMenu extends JFrame {
 		setUndecorated(true);
 		setTitle("Multi-Saufi");
 
-		JPanel bg = new JPanel();
-		bg.setLayout(new BoxLayout(bg, BoxLayout.Y_AXIS));
-		bg.setAlignmentX(JComponent.CENTER_ALIGNMENT);
-
-		JPanel button_jg_p= new JPanel();
-		JPanel button_hg_p= new JPanel();
-		JPanel button_op_p= new JPanel();
-		JPanel button_cr_p= new JPanel();
-		JPanel button_ex_p= new JPanel();
-		
+        
 		JButton button_jg = new JButton("Join Game");
 		JButton button_hg = new JButton("Host Game");
 		JButton button_op = new JButton("Options");
 		JButton button_cr = new JButton("Credits");
 		JButton button_ex = new JButton("Quit");
-		JPanel panel = new JPanel();
-		panel.setLayout(new GridLayout(0, 1));
-		//panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-		panel.setAlignmentX(JComponent.CENTER_ALIGNMENT);
-		panel.setAlignmentY(JComponent.CENTER_ALIGNMENT);
-        panel.setPreferredSize(new Dimension(100, 100));
-        panel.setMaximumSize(new Dimension(screenSize.width/4, screenSize.height/2));
-        panel.setBorder(new EmptyBorder(screenSize.height/4, 0, 0, 0));
-        
-        button_jg_p.setBorder(new EmptyBorder(0, 0, screenSize.height/16, 0));
-        button_hg_p.setBorder(new EmptyBorder(0, 0, screenSize.height/16, 0));
-        button_op_p.setBorder(new EmptyBorder(0, 0, screenSize.height/16, 0));
-        button_cr_p.setBorder(new EmptyBorder(0, 0, screenSize.height/16, 0));
 
-        button_jg_p.add(button_jg);
-        button_hg_p.add(button_hg);
-        button_op_p.add(button_op);
-        button_cr_p.add(button_cr);
-        button_ex_p.add(button_ex);
+		JPanel mainpanel = new JPanel();
+		mainpanel.setLayout(new GridBagLayout());
+		this.add(mainpanel);
+        mainpanel.setAlignmentX(JPanel.CENTER_ALIGNMENT);
 		
-		panel.add(button_jg_p);
-		panel.add(button_hg_p);
-		panel.add(button_op_p);
-		panel.add(button_cr_p);
-		panel.add(button_ex_p);
+		JPanel menupanel = new JPanel();
+		mainpanel.add(menupanel, new GridBagConstraints());
+		menupanel.setLayout(new BoxLayout(menupanel, BoxLayout.Y_AXIS));
+		menupanel.setAlignmentX(JPanel.CENTER_ALIGNMENT);
+		
+		menupanel.setPreferredSize(new Dimension(screenSize.width/4, screenSize.height/4));
+        menupanel.setMaximumSize(new Dimension(screenSize.width/4, screenSize.height/2));
+        menupanel.setBorder(BorderFactory.createLineBorder(Color.black));
+		
+        menupanel.add(button_jg);
+        menupanel.add(Box.createVerticalGlue());
+        menupanel.add(button_hg);
+        menupanel.add(Box.createVerticalGlue());
+        menupanel.add(button_op);
+        menupanel.add(Box.createVerticalGlue());
+        menupanel.add(button_cr);
+        menupanel.add(Box.createVerticalGlue());
+        menupanel.add(button_ex);
+        
+        button_jg.setAlignmentX(JButton.CENTER_ALIGNMENT);
+        button_hg.setAlignmentX(JButton.CENTER_ALIGNMENT);
+        button_op.setAlignmentX(JButton.CENTER_ALIGNMENT);
+        button_cr.setAlignmentX(JButton.CENTER_ALIGNMENT);
+        button_ex.setAlignmentX(JButton.CENTER_ALIGNMENT);
 		
 		button_jg.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
@@ -93,8 +95,6 @@ public class MainMenu extends JFrame {
             }
         });
 		
-		bg.add(panel);
-		getContentPane().add(bg);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		setVisible(true);
