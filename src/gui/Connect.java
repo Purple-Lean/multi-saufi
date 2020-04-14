775d458ca5aee7c504446d02e5436eeaecd15cde
Christian Brinkmann <christian.brinkmann09@gmail.com>
2020-04-14 15:31:46 +0200

    add connect window after joining a server

--- src/gui/Connect.java
+++ src/gui/Connect.java
@@ -4,13 +4,14 @@ import java.awt.Color;
 import java.awt.Component;
 import java.awt.Cursor;
 import java.awt.Dimension;
-import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.Point;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
@@ -20,28 +21,27 @@ import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JPasswordField;
-import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
-public class JoinGamePopup extends JFrame {
+public class Connect extends JFrame {
 
-	private static final long serialVersionUID = 5153752319840062711L;
-
-	static JoinGamePopup client = null;
-
-	static void start() {
-		if (client == null && HostGamePopup.host == null) {
-			client = new JoinGamePopup();
+	private static final long serialVersionUID = 5622599683122069424L;
+	
+	static Connect connect = null;
+	private static Timer timer;
+	
+	static void start(String address) {
+		if (connect == null) {
+			connect = new Connect(address);
 		}
 	}
 
-	public JoinGamePopup() {
+	public Connect(String address) {
 
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-
-		setSize(screenSize.width / 4, screenSize.height / 8);
-		setLocation(screenSize.width * 3 / 8, screenSize.height * 7 / 16);
+		
+		setSize(screenSize.width / 8, screenSize.height / 16);
+		setLocation(screenSize.width * 7 / 16, screenSize.height * 15 / 32);
 		// setExtendedState(JFrame.MAXIMIZED_BOTH);
 		setUndecorated(true);
 		setTitle("Multi-Saufi");
@@ -52,49 +52,24 @@ public class JoinGamePopup extends JFrame {
 		bg.setLayout(new BoxLayout(bg, BoxLayout.Y_AXIS));
 		bg.setAlignmentX(JComponent.CENTER_ALIGNMENT);
 
-		JLabel label = new JLabel("Address:");
-		label.setHorizontalAlignment(JLabel.CENTER);
-
-		JLabel label2 = new JLabel("Lobby-Password:");
-		label2.setHorizontalAlignment(JLabel.CENTER);
-
-		JTextField tf = new JTextField("ip/hostname:port", 100);
-
-		JPasswordField jp = new JPasswordField();
-
-		JPanel button_jo_p = new JPanel();
-		JPanel button_ex_p = new JPanel();
-
-		JButton button_jo = new JButton("Connect");
-		JButton button_ex = new JButton("Abort");
+		JLabel label = new JLabel("Trying to reach " + address);
+		JLabel label2 = new JLabel("Connecting     ");
+		
+		JButton button_ex = new JButton("Cancel");
 		JPanel panel = new JPanel();
-		panel.setLayout(new GridLayout(0, 2));
-		// panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+		label.setAlignmentX(JComponent.CENTER_ALIGNMENT);
+		label.setAlignmentY(JComponent.CENTER_ALIGNMENT);
+		label2.setAlignmentX(JComponent.CENTER_ALIGNMENT);
+		label2.setAlignmentY(JComponent.CENTER_ALIGNMENT);
+		button_ex.setAlignmentX(JComponent.CENTER_ALIGNMENT);
+		button_ex.setAlignmentY(JComponent.CENTER_ALIGNMENT);
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 		panel.setAlignmentX(JComponent.CENTER_ALIGNMENT);
 		panel.setAlignmentY(JComponent.CENTER_ALIGNMENT);
-		// panel.setPreferredSize(new Dimension(100, 100));
 
-		// button_ex_p.setBorder(new EmptyBorder(0, 0, screenSize.height/16, 0));
-		// button_jo_p.setBorder(new EmptyBorder(0, 0, screenSize.height/16, 0));
-
-		button_jo_p.add(button_jo);
-		button_ex_p.add(button_ex);
-
-		panel.add(label);
-		panel.add(tf);
-		panel.add(label2);
-		panel.add(jp);
-		panel.add(button_jo_p);
-		panel.add(button_ex_p);
-
-		button_jo.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				Component component = (Component) e.getSource();
-				JFrame frame = (JFrame) SwingUtilities.getRoot(component);
-				frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
-				Connect.start(tf.getText());
-			}
-		});
+		bg.add(label);
+		bg.add(label2);
+		bg.add(button_ex);
 
 		button_ex.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -104,16 +79,16 @@ public class JoinGamePopup extends JFrame {
 			}
 		});
 
-		panel.setBorder(BorderFactory.createLineBorder(Color.black));
-
-		bg.add(panel);
+		bg.setBorder(BorderFactory.createLineBorder(Color.black));
+		//bg.add(panel);
 		getContentPane().add(bg);
 		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		
+
 		addWindowListener(new java.awt.event.WindowAdapter() {
 			@Override
 			public void windowClosing(java.awt.event.WindowEvent windowEvent) {
-				JoinGamePopup.client = null;
+				Connect.connect = null;
+				timer.cancel();
 			}
 		});
 
@@ -121,12 +96,35 @@ public class JoinGamePopup extends JFrame {
 
 		changeCursor();
 		setIcon();
-
+		
+		timer = new Timer();
+		timer.scheduleAtFixedRate(new ScheduleTask(label2), 0l, 200l); 
+		
 	}
+		
+	private class ScheduleTask extends TimerTask {
+		
+		JLabel b = null;
+		
+		int counter = 0;
+		
+		public ScheduleTask(JLabel b) {
+			this.b = b;
+		}
+		
+        @Override
+        public void run() {
+            b.setText("Connecting"+new String(new char[counter]).replace("\0", ".")+new String(new char[5-counter]).replace("\0", " "));
+            counter++;
+            counter = counter%6;
+        }
+        
+    }
 
 	private void changeCursor() {
 		Toolkit toolkit = Toolkit.getDefaultToolkit();
-		Image image = Toolkit.getDefaultToolkit().getImage(getClass().getResource("../ressources/images/gui/cursor.png"));
+		Image image = Toolkit.getDefaultToolkit()
+				.getImage(getClass().getResource("../ressources/images/gui/cursor.png"));
 		Cursor c = toolkit.createCustomCursor(image, new Point(0, 0), "beer");
 		setCursor(c);
 	}
